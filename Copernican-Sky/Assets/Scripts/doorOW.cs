97d6e98478152b6d677b5dc35e1d2b140c99687e
MackHeller <mack.heller@mail.utoronto.ca>
2017-04-28 18:26:08 -0400

    started door

---
+++ Copernican-Sky/Assets/Scripts/doorOW.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class doorOW : MonoBehaviour {
+    public int level = 0;
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Mack"); 
+        if (collision.gameObject.name == "Hero")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
