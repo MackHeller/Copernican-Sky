0525acd31234c16f9942811db07d741e864990ca
MackHeller <mack.heller@mail.utoronto.ca>
2017-04-28 14:27:31 -0400

    added demo version, ball movement, walls and boxes

---
+++ Copernican-Sky/Assets/Scripts/heroOW.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heroOW : MonoBehaviour {
+    private float speed = 8.0f;
+    private float distance = 5f;
+    public Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        takeStep();
+	}
+
+    private void takeStep()
+    {
+        Vector2 newPos = new Vector2(0.0f,0.0f);
+        if (Input.GetKey(KeyCode.W))
+            newPos.y = distance;
+        if (Input.GetKey(KeyCode.S))
+            newPos.y = -distance;
+        if (Input.GetKey(KeyCode.A))
+            newPos.x = -distance;
+        if (Input.GetKey(KeyCode.D))
+            newPos.x = distance;
+        rb.AddForce(newPos * speed);
+    }
+}
