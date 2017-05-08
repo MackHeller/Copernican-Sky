1854b13f36d8a685c58649057676543a1d79bf18
MackHeller <mack.heller@mail.utoronto.ca>
2017-05-08 18:46:46 -0400

    allowed controller to persist between scenes

--- Copernican-Sky/Assets/Scripts/OverWorldController.cs
+++ Copernican-Sky/Assets/Scripts/OverWorldController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OverWorldController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake()
+    {
+       DontDestroyOnLoad(this);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
