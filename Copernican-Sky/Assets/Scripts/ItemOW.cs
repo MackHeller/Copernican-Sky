5c40a80aa889a6c6343373bf7893eb68abbbb841
MackHeller <mack.heller@mail.utoronto.ca>
2017-05-09 13:38:29 -0400

    chests open and cannot be opened again

--- Copernican-Sky/Assets/Scripts/ItemOW.cs
+++ Copernican-Sky/Assets/Scripts/ItemOW.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class ItemOW : MonoBehaviour
 {
     private OverWorldController overWorldController;
+    private SpriteRenderer spriteRenderer;
+    public Sprite altSprite;
     public int amount;
     public string itemName;
     private void Start()
     {
         overWorldController = GameObject.Find("GameController").GetComponent<OverWorldController>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     //TODO On contact add a *Item to the *Inventory
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Hero")
         {
-            overWorldController.addItemToInventory(overWorldController.getItemByName(itemName), amount);
-            Object.Destroy(this.gameObject);
+            //if chest is closed open it and get item
+            if(altSprite != spriteRenderer.sprite)
+            {
+                spriteRenderer.sprite = altSprite;
+                overWorldController.addItemToInventory(overWorldController.getItemByName(itemName), amount);
+            }
         }
     }
 }
