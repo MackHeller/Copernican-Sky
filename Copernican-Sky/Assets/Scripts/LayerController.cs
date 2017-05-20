f7ba2f882331a6cab48abf92757b6ef7670542fb
AustinLukeHeller <austin.heller@hotmail.ca>
2017-05-20 17:33:54 -0400

    Added in the layering script, mostly just to test git.

---
+++ Copernican-Sky/Assets/Scripts/LayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerController : MonoBehaviour {
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		// we are accessing the SpriteRenderer that is attached to the Gameobject
+		spriteRenderer = GetComponent<SpriteRenderer>(); 
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// this ties an object's sort order to its height on the screen
+		spriteRenderer.sortingOrder = -(int)(Mathf.Round(transform.position.y*100));
+
+	}
+}
