c97e67b8e6b09fb01667b2f5566be717e862c814
MackHeller <mack.heller@mail.utoronto.ca>
2017-06-10 18:33:22 -0400

    added enemy added leveling for hero

--- Copernican-Sky/Assets/Scripts/EnemyCB.cs
+++ Copernican-Sky/Assets/Scripts/EnemyCB.cs
@@ -12,5 +12,32 @@
 
 public class Personality
 {
-     
+    public Personality buildItem(PersonalityTypes type)
+    {
+        switch (type)
+        {
+            case PersonalityTypes.AGGRESSIVE:
+                return new Personality();
+            case PersonalityTypes.PASSIVE:
+                return new Personality();
+            case PersonalityTypes.RECKLESS:
+                return new Personality();
+            case PersonalityTypes.TIMID:
+                return new Personality();
+            default:
+                throw new System.Exception();
+        }
+    }
+    private Personality()
+    {
+
+    }
+}
+//TODO make more types
+public enum PersonalityTypes
+{
+    AGGRESSIVE,
+    PASSIVE,
+    RECKLESS,
+    TIMID
 }
--- Copernican-Sky/Assets/Scripts/HeroCB.cs
+++ Copernican-Sky/Assets/Scripts/HeroCB.cs
@@ -1,17 +1,35 @@
-﻿/**
+﻿using System;
+/**
  * This is the hero class (one of three) that contains most of stats and information for the playable characters.
  * While it is primarily used by the combat controller, the other two controllers will also use this class to get
  * character information when they need it (for ex. party character names.) 
  * */
 public class HeroCB  {
-    Skills playerSkills;
-    string name;
-    Character character;
+    private Skills playerSkills;
+    private string name;
+    private Character character;
+    private int expirence;
     public HeroCB(string name, Character character, Skills playerSkills)
     {
         this.name = name;
         this.character = character;
         this.playerSkills = playerSkills;
+    }
+    public int getExpirence()
+    {
+        return expirence;
+    }
+    public int getLevel()
+    {
+        return (int)Math.Ceiling(130 * Math.Tanh(expirence / 1200));
+    }
+    public int getExpirenceTillNextLevel()
+    {
+        return (int)Math.Ceiling(1200 * ATanh(getLevel() + 1) / 130);
+    }
+    private static double ATanh(double x)
+    {
+        return (Math.Log(1 + x) - Math.Log(1 - x)) / 2;
     }
 }
 
