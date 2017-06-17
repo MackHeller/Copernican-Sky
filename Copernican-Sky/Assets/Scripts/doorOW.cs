c0f154b322ede3b8a57c77e6ea578a5d31c1ef59
MackHeller <mack.heller@mail.utoronto.ca>
2017-06-17 16:33:36 -0400

    fixed doors added menu scene to builds

--- Copernican-Sky/Assets/Scripts/doorOW.cs
+++ Copernican-Sky/Assets/Scripts/doorOW.cs
@@ -2,14 +2,14 @@
 using UnityEngine.SceneManagement;
 
 public class doorOW : MonoBehaviour {
-    public int level = 0;
+    public string sceneToTravelTo = "";
     // Update is called once per frame
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Mack");
         if (collision.gameObject.name == "Hero")
         {
-            SceneManager.LoadScene(level, LoadSceneMode.Single);
+            SceneManager.LoadScene(sceneToTravelTo, LoadSceneMode.Single);
         }
     }
     
