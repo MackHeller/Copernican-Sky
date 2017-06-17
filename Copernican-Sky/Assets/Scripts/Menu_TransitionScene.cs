d43b4e85ad67660ba930f424dd5eb31965b3604c
Patrick Dober <claytonwjennings@gmail.com>
2017-06-17 16:06:26 -0400

    Beginning of main menu assets, currently not working

---
+++ Copernican-Sky/Assets/Scripts/Menu_TransitionScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Menu_TransitionScene : MonoBehaviour {
+
+    public void LoadNextIndex(string currentButton)
+    {
+        Debug.Log(currentButton);
+        string yup = SceneManager.GetActiveScene().name;
+        if (SceneManager.GetActiveScene().name == "StartMenu")
+        {
+            Debug.Log("start");
+            LoadNextScene("OptionScene", "OptionScene", currentButton);
+        }
+        else if (SceneManager.GetActiveScene().name == "OptionMenu")
+        {
+            LoadNextScene("StartMenu", "StartMenu" ,currentButton);
+        }
+
+    }
+    public void LoadNextScene(string next, string previous, string but)
+    {
+        if (but == "Right")
+        {
+            Scene temp = SceneManager.GetSceneByName(next);
+            Debug.Log(temp.name);
+            if (temp.isLoaded == false)
+            {
+                SceneManager.LoadScene(temp.name);
+            }
+            Debug.Log(temp.isLoaded);
+            SceneManager.SetActiveScene(temp);
+
+        }
+        else if (but == "Left")
+        {
+            Scene temp = SceneManager.GetSceneByName(previous);
+            Debug.Log(temp.isLoaded);
+            if (temp.isLoaded == false)
+            {
+                SceneManager.LoadScene(temp.name);
+            }
+            SceneManager.SetActiveScene(temp);
+        }
+    }
+}
